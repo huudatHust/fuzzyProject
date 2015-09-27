bfc1b3ea01e854972dc3f29a63d60691291e4c35
huudatHust <datk571102@gmail.com>
2015-09-27 20:55:36 +0700

    - update 29-04-15

--- src/fuzzyproject/FuzzyProject.java
+++ src/fuzzyproject/FuzzyProject.java
@@ -10,7 +10,7 @@ public class FuzzyProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+        System.out.println("hello world");
     }
     
 }
